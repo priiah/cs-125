f08e57ddb093b5ea0c06801d9758e58d7f979ff5
Farah Ahmed <fpahmed@uci.edu>
2023-02-09 20:01:25 -0800

    created basics of app with tabs, titles and icons

---
+++ walk-it-off/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Walk It Off!',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
