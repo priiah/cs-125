a6829e8dd5359b32a3328a1d4b959f00acf9a69f
Farah Ahmed <fpahmed@uci.edu>
2023-02-10 00:35:09 -0800

    made headers nicer

--- walk-it-off/src/app/tab4/tab4.module.ts
+++ walk-it-off/src/app/tab4/tab4.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { Tab4Page } from './tab4.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
-import { Tab3PageRoutingModule } from './tab4-routing.module';
+import { Tab4PageRoutingModule } from './tab4-routing.module';
 
 @NgModule({
   imports: [
@@ -13,7 +13,7 @@ import { Tab3PageRoutingModule } from './tab4-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab3PageRoutingModule
+    Tab4PageRoutingModule
   ],
   declarations: [Tab4Page]
 })
